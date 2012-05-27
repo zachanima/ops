1eb28ee76c67e3cd10a6326f3d0718296c27afb3
Zach Ewert <zachanima@gmail.com>
2012-05-27 10:33:25 +0200

    Color pilots by activity in operations index view

--- app/helpers/operations_helper.rb
+++ app/helpers/operations_helper.rb
@@ -1,2 +1,18 @@
 module OperationsHelper
+  def pilots_by_activity activities
+    activities.collect do |activity|
+      content_tag :span, :class =>
+        if activity.operated
+          if activity.prepared
+            'operated prepared'
+          else
+            'operated'
+          end
+        else
+          'prepared'
+        end do
+        h activity.pilot.first_name
+      end
+    end * ', '
+  end
 end
